9175333fd69de7a4dfba5635733c70db2789d15d
Joe Brentnall <joe.brentnall25@gmail.com>
2024-02-18 18:50:29 +0000

    Added routing and blank pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
+import Menus from "./pages/Menus/Menus";
+import NotFound from "./pages/NotFound/NotFound";
 
 
 const App: React.FC = () => {
-    return (
+  return (
+    <BrowserRouter>
       <div>
-        <Home/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/menus" element={<Menus />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </div>
-    );
-  };
-  
-  export default App;
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/pages/Menus/Menus.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Menus: React.FC = () => {
+    return (
+      <div>
+        MENUS   
+      </div>
+    );
+  };
+  
+export default Menus;
