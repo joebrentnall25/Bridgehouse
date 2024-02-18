feba363a7407c53ab6012dc4a766fbdae27e2020
Joe Brentnall <joe.brentnall25@gmail.com>
2024-02-18 20:02:02 +0000

    Added button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,25 +1,57 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export enum ButtonTypeEnum {
     primary = "primary",
-    secondary = "secondary",
-    tertiary = "tertiary"
+    secondary = "secondary"
 }
 
 interface ButtonProps {
     type: ButtonTypeEnum;
     text: string;
+    emphasis?: string;
+    callback?: () => any;
+    linkTo?: string;
 }
 
 const Button: React.FC<ButtonProps> = ({
     type,
-    text
+    text,
+    emphasis,
+    callback,
+    linkTo
 }: ButtonProps) => {
-    if (type === ButtonTypeEnum.primary) {
-        return <button style={{ backgroundColor: "transparent", padding: "10px 20px", borderRadius: "10px", border: "1px solid black" }}>{text}</button>
+    const chooseStyle = (): object => {
+        const borderRadius = "27px"
+        const padding = "20px 60px"
+        let style: {[key: string]: any} = {
+            padding: padding, 
+            borderRadius: borderRadius,
+        }
+        if (emphasis) {
+            style["fontWeight"] = emphasis
+        }
+        if (type === ButtonTypeEnum.primary) {
+            return { ...style, backgroundColor: "#5C1F37", border: "1px solid #5C1F37", color: "white"};
+        } else if (type === ButtonTypeEnum.secondary) {
+            return { ...style, backgroundColor: "transparent", border: "2px solid black", color: "black"};;
+        }
+        return style
     }
-    return <button>Error invalid button type</button>
+
+    const getButtonComponent = () => {
+        return (
+            <button style={chooseStyle()} onClick={callback}>{text}</button>
+        )
+    }
+
+    return (
+        <>
+            {linkTo ? <Link to={linkTo}>{getButtonComponent()}</Link> : getButtonComponent()}
+        </>
+    )
+
 
 };
-  
+
 export default Button;
